ccd44a72fe47db8c444d4f1975e3892ff89460a7
Thanmai <152688241+thanmaikrishna@users.noreply.github.com>
2025-01-23 23:18:29 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//   mongoURI: 'mongodb://localhost:27017/video-annotation-app',
+// };
+require('dotenv').config();
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/video-annotation-app',
+  mongoURI: process.env.MONGO_URI,
 };
